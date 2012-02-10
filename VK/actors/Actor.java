38d9bc1d4c7ef5204f2db315d921acd694c2fc35
Pim Vellinga <pimvellinga@gmail.com>
2012-02-10 10:36:00 +0100

    Javadoc

--- VK/actors/Actor.java
+++ VK/actors/Actor.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import VK.view.Field;
 import VK.view.Location;
+
 /**
  * The interface Actor 
  * Provides methods that should be implemented 
  */
+
 public interface Actor {
 	
 	public boolean alive = true;
